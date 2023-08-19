1e923b780796c981a4a64d05e4feac86baa7efc3
dengliang <1650465505@qq.com>
2023-08-19 09:25:29 +0800

    2-04-Nacos配置管理-配置热更新-value+refreshScope 方式

--- cloud-demo/user-service/src/main/java/cn/itcast/user/service/UserService.java
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/service/UserService.java
@@ -3,15 +3,23 @@ package cn.itcast.user.service;
 import cn.itcast.user.mapper.UserMapper;
 import cn.itcast.user.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 @Service
+@RefreshScope
 public class UserService {
+
+    @Value("${user.suffix}")
+    String userSuffix;
 
     @Autowired
     private UserMapper userMapper;
 
     public User queryById(Long id) {
-        return userMapper.findById(id);
+        User byId = userMapper.findById(id);
+        byId.setUsername(byId.getUsername()+"用户统一后缀为 "+userSuffix);
+        return byId;
     }
 }
