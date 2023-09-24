fd4bef19cb705510466b0117eeef761b32900131
dengliang <1650465505@qq.com>
2023-09-24 12:03:32 +0800

    【spring-cloud】增加spring boot获取request方式

--- cloud-demo/order-service/src/main/java/cn/itcast/order/web/FeignOrderController.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/web/FeignOrderController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("feignOrder")
+@CrossOrigin
 public class FeignOrderController {
     @Autowired
     GlobalConfig globalConfig;
@@ -23,8 +26,9 @@ public class FeignOrderController {
     UserClient userClient;
 
     @GetMapping("{userID}")
-    public User queryOrderAndUser(@PathVariable("userID") Long userID){
+    public User queryOrderAndUser(@PathVariable("userID") Long userID,HttpServletRequest servletRequest){
         User byId = userClient.findById(userID);
+        String realPath = servletRequest.getServletContext().getRealPath("");
         byId.setUsername(byId.getUsername()+  LocalDateTime.now().format(DateTimeFormatter.ofPattern(globalConfig.getTimefo()))  );
         return byId;
     }
